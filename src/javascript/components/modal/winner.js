5f28d0333ad25d734ab7dbc50a11c0f3c69df7bb
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2023-06-23 04:18:02 +0300

    + added image of the winner

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,10 +1,21 @@
 import showModal from './modal';
+import createElement from '../../helpers/domHelper';
 
 export default function showWinnerModal(fighter) {
     // call showModal function
-    const title = 'Winner:';
-    const bodyElement = document.createElement('p');
-    bodyElement.innerText = `${fighter.name} is the winner!`;
+    const title = `Winner: ${fighter.name}`;
+    const bodyElement = document.createElement('div');
+
+    const winnerImage = createElement({
+        tagName: 'img',
+        className: 'modal-winner-img',
+        attributes: {
+            src: fighter.source,
+            alt: fighter.name
+        }
+    });
+
+    bodyElement.append(winnerImage);
 
     showModal({ title, bodyElement });
 }
